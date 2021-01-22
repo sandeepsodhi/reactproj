8cfdb4d35de370da47ccd33d4ee02bbb0c08d1b4
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-01-22 06:11:41 -0500

    Compiled Code form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>HELLO!! </h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>HELLO!! </h1>
+  //   </div>
+  // );
+  return React.createElement("div","",React.createElement("h1",{className:'App'},"Create New Element within h1!!!"));
 }
 
 export default App;
