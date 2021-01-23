9675c50f276c93ff4b6aaf025dac909498569d62
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-01-23 11:32:27 -0500

    Extending component and creating a new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import React from 'react'
+import React, { Component } from 'react';
+import Person from './Person/Person'; //when importing this person we must make sure that we are importing with capital letter, reason for this becasue there are reserved words which we can not keep like div/class etc.
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>HELLO!! </h1>
-  //   </div>
-  // );
-  return React.createElement("div","",React.createElement("h1",{className:'App'},"Create New Element within h1!!!"));
+//we can have only one root element in a componnet
+
+class App extends Component{ 
+  render() {
+      return(
+        <div className="App" >
+          <h1>HELLO!! </h1>
+          <Person />
+        </div>
+      );
+    }
+  //retur n React.createElement("div","",React.createElement("h1",{className:'App'},"Create New Element within h1!!!"));
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+//following es6 syntax to create a function
+//cont->not planing to change this function, person->functionName, () -> parameterList for function
+
+//import React from 'react';
+
+const person = () => {
+    return <h2>I am a Person</h2>
+};
+
+export default person;
