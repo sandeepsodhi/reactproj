6e63982711c4abca9e25697fa7e8dd35bbb97288
Sandeep Sodhi <vickysodhi15@gmail.com>
2021-02-02 22:13:24 -0500

    changing statless components to statefull componentss

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,34 @@
 //import React from 'react';
-
+import { Component } from 'react';
 import './Person.css'; 
 
-const person = (props) => {
 
-    const rnd = Math.random();
-
-    if(rnd > 0.7){
-        //throw new Error('Something went wrong.')
+class Person extends Component{
+    render(){
+        return (
+            <div className="Person"> 
+                <p onClick={this.props.click}> I am a {this.props.name}, and I am  {this.props.age} old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value = {this.props.name} /> 
+            </div>
+        );
     }
 
-    return (
-        <div className="Person"> 
-            <p onClick={props.click}> I am a {props.name}, and I am  {props.age} old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value = {props.name} /> 
-        </div>
-    );
+}
 
-};
+export default Person;
 
-export default person;
+// const person = (props) => {
+//     const rnd = Math.random();
+//     if(rnd > 0.7){
+//         //throw new Error('Something went wrong.')
+//     }
+//     return (
+//         <div className="Person"> 
+//             <p onClick={props.click}> I am a {props.name}, and I am  {props.age} old</p>
+//             <p>{props.children}</p>
+//             <input type="text" onChange={props.changed} value = {props.name} /> 
+//         </div>
+//     );
+// };
+// export default person;
